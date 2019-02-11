213392d25336a1a5b9ce824477b2a718b03a3cf3
sarah-am <sarahq.aman@gmail.com>
2019-02-11 18:46:03 +0300

    passed test

--- api/serializers.py
+++ api/serializers.py
@@ -2,16 +2,52 @@ from rest_framework import serializers
 from restaurants.models import Restaurant
 
 class RestaurantListSerializer(serializers.ModelSerializer):
+    detail = serializers.HyperlinkedIdentityField(
+        view_name = 'api-detail',
+        lookup_field = 'id',
+        lookup_url_kwarg = 'restaurant_id'
+        )
+
+    update = serializers.HyperlinkedIdentityField(
+        view_name = 'api-update',
+        lookup_field = 'id',
+        lookup_url_kwarg = 'restaurant_id'
+        )
+
+    delete = serializers.HyperlinkedIdentityField(
+        view_name = 'api-delete',
+        lookup_field = 'id',
+        lookup_url_kwarg = 'restaurant_id'
+        )    
+
+
     class Meta:
         model = Restaurant
         fields = [
             'name',
             'opening_time',
             'closing_time',
+            'detail',
+            'update',
+            'delete'
             ]
 
+# command B to find bug
+# keep detail, update and delete before class meta and be mindful of indentation!
 
 class RestaurantDetailSerializer(serializers.ModelSerializer):
+    update = serializers.HyperlinkedIdentityField(
+        view_name = 'api-update',
+        lookup_field = 'id',
+        lookup_url_kwarg = 'restaurant_id'
+        )
+
+    delete = serializers.HyperlinkedIdentityField(
+        view_name = 'api-delete',
+        lookup_field = 'id',
+        lookup_url_kwarg = 'restaurant_id'
+        )        
+
     class Meta:
         model = Restaurant
         fields = [
@@ -21,7 +57,10 @@ class RestaurantDetailSerializer(serializers.ModelSerializer):
             'description',
             'opening_time',
             'closing_time',
+            'update',
+            'delete'            
             ]
+  
 
 class RestaurantCreateUpdateSerializer(serializers.ModelSerializer):
     class Meta:
